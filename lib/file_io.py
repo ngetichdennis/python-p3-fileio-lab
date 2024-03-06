1e045e17e51ebc27dddc56dcda67025c381c6666
Dennis <ngetichdennis90@gmail.com>
2024-03-06 15:00:35 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name="test_file", file_content="This is a test content."):
+    with open(f"{file_name}.txt", mode='w',encoding="utf-8") as file:
+        file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name="test_file", file_content="This is a test content."):
+    with open(f"{file_name}.txt",mode= 'a') as file:
+        file.write(file_content)
 
-def read_file(file_name):
-    pass
+def read_file(file_name="test_file"):
+    with open(f"{file_name}.txt", 'r') as file:
+        return file.read()
